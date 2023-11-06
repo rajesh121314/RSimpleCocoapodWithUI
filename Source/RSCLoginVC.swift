f9ea96343dffd245ed0a49d335d6c67e7acbd5cb
Rajesh <rajesh.s@vrinsoft.com>
2023-11-06 17:27:10 +0530

    added loginUI

---
+++ Source/RSCLoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  RSCLoginVC.swift
+//  RSimpleCocoapodWithUI
+//
+//  Created by vrinsoft on 06/11/23.
+//
+
+import UIKit
+
+class RSCLoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
