135bf72891d9e939e1ba95f8016b81c1ac7f62b4
Rajesh <rajesh.s@vrinsoft.com>
2023-11-06 18:38:13 +0530

    added resource media

--- Example/RSimpleCocoapodWithUI/ViewController.swift
+++ Example/RSimpleCocoapodWithUI/ViewController.swift
@@ -10,22 +10,30 @@ import UIKit
 import RSimpleCocoapodWithUI
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgTopImage: UIImageView!
+    
     let objRSCLoginVC = RSCLoginVC()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgTopImage.image = loadLoginImage(imageName: "imgLogIn")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func btnAction_loginClicked(_ sender: UIButton) {
         present(objRSCLoginVC, animated: true)
     }
     
+    func loadLoginImage(imageName: String) -> UIImage?  {
+        let bundle: Bundle = Bundle(for: RSCLoginVC.self)
+        return UIImage(named: imageName, in: bundle, compatibleWith: nil)
+        
+    }
 }
 
